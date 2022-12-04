9a265cfbd3fc54d53b7a9d41b65e22e858c58754
Oksana-laura <oksanashram@gmail.com>
2022-12-04 17:45:15 +0300

    добавлены views, seriealizers, permissions

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 # from rest_framework.validators import UniqueTogetherValidato
 
-from reviews.models import Categories, Genres, Titles
+from reviews.models import Categories, Comment, Genres, Review, Titles
 
 
 class TitlesSerializer(serializers.ModelSerializer):
     genre = serializers.StringRelatedField(many=True, read_only=True)
     category = SlugRelatedField(slug_field='name', read_only=True)
+
     class Meta:
         fields = ('name', 'year', 'description', 'genre', 'category',)
         model = Titles
@@ -24,4 +25,41 @@ class CategoriesSerializer(serializers.ModelSerializer):
         fields = '__all__'
         model = Categories
 
- 
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+    title = serializers.SlugRelatedField(
+        read_only=True, slug_field='name'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Review
+
+    def validate(self, data):
+        if self.context['request'].method != 'POST':
+            author = self.context['request'].user
+            title_id = self.context['view'].kwargs['title_id']
+        if Review.objects.filter(
+            author=author,
+            title_id=title_id
+        ).exists():
+            raise serializers.ValidationError(
+                'Вы уже оставляли отзыв к этому произведению.'
+            )
+        return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+    review = serializers.SlugRelatedField(
+        read_only=True, slug_field='text'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,17 +1,32 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import TitlesViewSet, CategoriesViewSet
+from .views import (
+    TitlesViewSet,
+    CategoriesViewSet,
+    ReviewViewSet,
+    CommentViewSet
+)
 
+app_name = 'api'
 
 routerV1 = DefaultRouter()
 routerV1.register('titles', TitlesViewSet, basename='titles')
 routerV1.register('categories', CategoriesViewSet, basename='categories')
+routerV1.register(
+    r'titles/(?P<title_id>[1-9]\d*)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+routerV1.register(
+    r'titles/(?P<title_id>[1-9]\d*)/reviews/(?P<review_id>[1-9]\d*)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
-# routerV1.register('groups', GroupViewSet, basename='groups')
-# vrouterV1.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
-    path('', include(routerV1.urls)),
-    # path('v1/', include('djoser.urls.jwt')),
+    path('api/v1/', include(routerV1.urls)),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.jwt')),
 ]
